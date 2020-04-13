e9695a0dbd9bb829045babd6ecfdf98f5797704a
Zsofia <oroszi.zsofi@gmail.com>
2020-04-13 13:18:47 +0200

    display 10 initial jokes

--- src/JokeApp.js
+++ src/JokeApp.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import Jokes from './Jokes';
+import Joke from './Joke';
 import './JokeApp.css';
 import axios from "axios";
 const API_URL = 'https://icanhazdadjoke.com/';
 
 class JokeApp extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    }
     constructor(props){
         super(props);
         this.state = { jokes: [] };
     }
 
     async componentDidMount(){
-        let res = await axios.get(API_URL, {
-            headers: { Accept: "application/json" }
-            });
-        console.log(res.data.joke)
+        let jokes = [];
+        while(jokes.length < this.props.numJokesToGet){
+            let res = await axios.get(API_URL, {
+                headers: { Accept: "application/json" }
+                });
+            jokes.push(res.data.joke)
+        }
+        this.setState({ jokes: jokes});
+        console.log(this.state.jokes);
     }
 
     render(){
         return(
             <div className="JokeApp">
                 <h1>Dad Jokes</h1>
+                <div>
+                    {this.state.jokes.map(j => (
+                        <div>{j}</div>
+                    ))}
+                </div>
                 <button>New Jokes</button>
-                <Jokes />
+                <Joke />
             </div>
         )
     }
