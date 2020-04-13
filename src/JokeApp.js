df9eaecb34a97bb791bf828614a5280cd1be3102
Zsofia <oroszi.zsofi@gmail.com>
2020-04-13 12:51:20 +0200

    fetch data from API

--- src/JokeApp.js
+++ src/JokeApp.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import Jokes from './Jokes';
 import './JokeApp.css';
+import axios from "axios";
+const API_URL = 'https://icanhazdadjoke.com/';
 
 class JokeApp extends Component {
+    constructor(props){
+        super(props);
+        this.state = { jokes: [] };
+    }
+
+    async componentDidMount(){
+        let res = await axios.get(API_URL, {
+            headers: { Accept: "application/json" }
+            });
+        console.log(res.data.joke)
+    }
+
     render(){
         return(
             <div className="JokeApp">
-                <h1>Hello from the JokeApp</h1>
+                <h1>Dad Jokes</h1>
+                <button>New Jokes</button>
                 <Jokes />
             </div>
         )
