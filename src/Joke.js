6c508e7be182cf2937bb932311fe1e5dcb0fdb5f
Zsofia <oroszi.zsofi@gmail.com>
2020-04-17 18:26:25 +0200

    style Joke component

--- src/Joke.js
+++ src/Joke.js
@@ -7,10 +7,13 @@ class Joke extends Component {
             <div className="Joke">
                 <div className="Joke-buttons">
                     <i class="fas fa-arrow-up" onClick={this.props.upvote}></i>
-                    <span>{this.props.votes}</span>
+                    <span className="Joke-votes">{this.props.votes}</span>
                     <i class="fas fa-arrow-down" onClick={this.props.downvote}></i>
                 </div>
                 <div className="Joke-text">{this.props.text}</div>
+                <div className="Joke-smiley">
+                    <i class="em em-rolling_on_the_floor_laughing"></i>
+                </div>
             </div>
         )
     }
