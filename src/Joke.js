cbdfa4c84c8685725c26c14558303824e35eef7d
Zsofia <oroszi.zsofi@gmail.com>
2020-04-17 18:46:01 +0200

    add dynamic emojis by vote score

--- src/Joke.js
+++ src/Joke.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './Joke.css';
 
 class Joke extends Component {
-
     getColor(){
         if(this.props.votes >= 15){
             return "#4CAF50"
@@ -20,14 +19,23 @@ class Joke extends Component {
             return "#f44336"
         }
     }
-
-    "em em-rolling_on_the_floor_laughing"
-    "em em-laughing"
-    "em em-smiley"
-    "em em-slightly_smiling_face"
-    "em em-neutral_face"
-    "em em-confused"
-    "em em-angry"
+    getEmoji(){
+        if(this.props.votes >= 15){
+            return "em em-rolling_on_the_floor_laughing"
+        } else if(this.props.votes >= 12){
+            return "em em-laughing"
+        } else if(this.props.votes >= 9){
+            return "em em-smiley"
+        } else if(this.props.votes >= 6){
+            return "em em-slightly_smiling_face"
+        } else if(this.props.votes >= 3){
+            return "em em-neutral_face"
+        } else if(this.props.votes >= 0){
+            return "em em-confused"
+        } else {
+            return "em em-angry"
+        }
+    }
 
     render(){
         return(
@@ -39,7 +47,7 @@ class Joke extends Component {
                 </div>
                 <div className="Joke-text">{this.props.text}</div>
                 <div className="Joke-smiley">
-                    <i class="em em-rolling_on_the_floor_laughing"></i>
+                    <i className={this.getEmoji()}></i>
                 </div>
             </div>
         )
